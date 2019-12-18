0aa0d55cf80abd2d2ff81264ae2f8dd55ab3a1cb
Abai Abakirov <abaikirov@gmail.com>
2019-12-18 10:43:47 +0600

    initial commit

---
+++ UDEX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UDEX
+//
+//  Created by Abai Abakirov on 12/17/19.
+//  Copyright © 2019 MakeUseOf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
