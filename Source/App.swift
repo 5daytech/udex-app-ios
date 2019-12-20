def831b268572b41087f4fa551997c6e641c7c45
Abai Abakirov <abaikirov@gmail.com>
2019-12-20 11:02:01 +0600

    coins

--- Source/App.swift
+++ Source/App.swift
@@ -19,14 +19,67 @@ class App {
   let zrxNetwork = ZrxKit.NetworkType.Ropsten
   
   static let coins: [Coin] = [
-    Coin(title: "Ethereum", code: "ETH", coinType: .Ethereum),
-    Coin(title: "Wrapped ETH", code: "WETH", coinType: .Erc20(address: "0xc778417e063141139fce010982780140aa0cd5ab", decimal: 18)),
-    Coin(title: "0x", code: "ZRX", coinType: .Erc20(address: "0xff67881f8d12f372d91baae9752eb3631ff0ed00", decimal: 18)),
-    Coin(title: "Wrapped Bitcoin", code: "WBTC", coinType: .Erc20(address: "0x96639968b1da3438dbb618465bcb2bf7b25ee6ad", decimal: 18)),
-    Coin(title: "Dai", code: "DAI", coinType: .Erc20(address: "0xd914796ec26edd3f9651393f9751e0f3c00dd027", decimal: 18)), // It's CHO
-    Coin(title: "ChainLink", code: "LINK", coinType: .Erc20(address: "0x30845a385581ce1dc51d651ff74689d7f4415146", decimal: 18)), // It's TMKV2
-    Coin(title: "Tether USD", code: "USDT", coinType: .Erc20(address: "0x6D00364318D008C3AEA08c097c25F5639AB5D2e6", decimal: 3)), // It's PPA
-    Coin(title: "Huobi", code: "HT", coinType: .Erc20(address: "0x52E64BB7aEE0E5bdd3a1995E3b070e012277c0fd", decimal: 2)) // It's TMK
+    Coin(
+      title: "Ethereum",
+      code: "ETH",
+      coinType: .Ethereum
+    ),
+    Coin(
+      title: "Wrapped ETH",
+      code: "WETH",
+      coinType: .Erc20(
+        address: "0xc778417e063141139fce010982780140aa0cd5ab",
+        decimal: 18
+      )
+    ),
+    Coin(
+      title: "0x",
+      code: "ZRX",
+      coinType: .Erc20(
+        address: "0xff67881f8d12f372d91baae9752eb3631ff0ed00",
+        decimal: 18
+      )
+    ),
+    Coin(
+      title: "Wrapped Bitcoin",
+      code: "WBTC",
+      coinType: .Erc20(
+        address: "0x96639968b1da3438dbb618465bcb2bf7b25ee6ad",
+        decimal: 18
+      )
+    ),
+    Coin(
+      title: "Dai",
+      code: "DAI",
+      coinType: .Erc20(
+        address: "0xd914796ec26edd3f9651393f9751e0f3c00dd027",
+        decimal: 18
+      )
+    ), // It's CHO
+    Coin(
+      title: "ChainLink",
+      code: "LINK",
+      coinType: .Erc20(
+        address: "0x30845a385581ce1dc51d651ff74689d7f4415146",
+        decimal: 18
+      )
+    ), // It's TMKV2
+    Coin(
+      title: "Tether USD",
+      code: "USDT",
+      coinType: .Erc20(
+        address: "0x6D00364318D008C3AEA08c097c25F5639AB5D2e6",
+        decimal: 3
+      )
+    ), // It's PPA
+    Coin(
+      title: "Huobi",
+      code: "HT",
+      coinType: .Erc20(
+        address: "0x52E64BB7aEE0E5bdd3a1995E3b070e012277c0fd",
+        decimal: 2
+      )
+    ) // It's TMK
   ]
   
   static let exchangePairs: [Pair<AssetItem, AssetItem>] = [
@@ -54,7 +107,8 @@ class App {
         availablePairs: App.exchangePairs,
         feeRecipients: ["0xA5004C8b2D64AD08A80d33Ad000820d63aa2cCC9".lowercased()],
         exchangeAddress: zrxNetwork.exchangeAddress,
-        config: relayerConfig)
+        config: relayerConfig
+      )
     ]
     
     let words = "burden crumble violin flip multiply above usual dinner eight unusual clay identify".split(separator: " ").map { String($0) }
