70df28b53d819d8a17608c368115b6b102f4f8d1
Abai Abakirov <abaikirov@gmail.com>
2019-12-30 10:37:42 +0600

    fix input behavior

--- Source/Modules/Main/MainView.swift
+++ Source/Modules/Main/MainView.swift
@@ -1,44 +1,38 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-      TabView {
-        NavigationView {
-          BalanceView(
-            viewModel: BalanceViewModel(
-              adapterManager: App.instance.adapterManager,
-              coins: App.instance.appConfigProvider.coins
-            )
+  var body: some View {
+    TabView {
+      NavigationView {
+        BalanceView(
+          viewModel: BalanceViewModel(
+            adapterManager: App.instance.adapterManager,
+            coins: App.instance.appConfigProvider.coins
           )
+        )
           .navigationBarTitle(Text("Balance"))
-        }
-        .tabItem({
-          Text("Balance")
-        })
-        NavigationView {
-          OrdersList(
-            viewModel: OrdersViewModel(
-              relayerAdapter: App.instance.relayerAdapterManager.mainRelayer
-            )
-          )
-          .navigationBarTitle(Text("Order book"))
-        }
-        .tabItem({
-          Text("Order book")
-        })
-        NavigationView {
-          ExchangeView(viewModel: ExchangeViewModel())
+      }
+      .tabItem({
+        Text("Balance")
+      })
+      OrdersList(
+        viewModel: OrdersViewModel(
+          relayerAdapter: App.instance.relayerAdapterManager.mainRelayer
+        )
+      )
+      NavigationView {
+        ExchangeView(viewModel: ExchangeViewModel())
           .navigationBarTitle(Text("Exchange"))
-        }
-        .tabItem {
-          Text("Exchange")
-        }
+      }
+      .tabItem {
+        Text("Exchange")
       }
     }
+  }
 }
 
 struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
-    }
+  static var previews: some View {
+    MainView()
+  }
 }
--- Source/Modules/Orders/View/OrdersList.swift
+++ Source/Modules/Orders/View/OrdersList.swift
@@ -3,37 +3,54 @@ import SwiftUI
 struct OrdersList: View {
   @ObservedObject var viewModel: OrdersViewModel
   
+  let ordersSpacing: CGFloat = 30
+  
   var body: some View {
-    VStack {
-      ZStack(alignment: .top) {
-        HStack(alignment: .center, spacing: 0) {
-          List(viewModel.buyOrders) { order in
-            OrderRow(order: order)
-          }
-          List(viewModel.sellOrders) { order in
-            OrderRow(order: order)
-          }
-        }
-        .offset(x: 0, y: 50)
-        
-        HStack(alignment: .top) {
-          Text("Current Pair")
-            .offset(x: 0, y: 12)
-          Spacer()
-          List(viewModel.availablePairs) { pair in
-            Button(action: {
-              self.viewModel.onChoosePair(pair: pair)
-            }) {
-              HStack {
-                Text(pair.baseCoin)
-                Spacer()
-                Text(pair.quoteCoin)
+    NavigationView {
+      VStack {
+        ZStack(alignment: .top) {
+          ScrollView {
+            HStack(alignment: .bottom, spacing: ordersSpacing) {
+              VStack(alignment: .center, spacing: ordersSpacing) {
+                ForEach(viewModel.buyOrders) { order in
+                  OrderRow(order: order)
+                }
+              }
+              VStack(alignment: .center, spacing: ordersSpacing) {
+                ForEach(viewModel.sellOrders) { order in
+                  OrderRow(order: order)
+                }
               }
             }
           }
-          .frame(height: viewModel.isExpanded ? 400 : 40)
+          .offset(x: 0, y: 50)
+          
+          HStack(alignment: .top) {
+            Text("Current Pair")
+              .offset(x: 0, y: 12)
+            Spacer()
+            List(viewModel.availablePairs) { pair in
+              Button(action: {
+                self.viewModel.onChoosePair(pair: pair)
+              }) {
+                HStack {
+                  Text(pair.baseCoin)
+                  Spacer()
+                  Text(pair.quoteCoin)
+                }
+              }
+            }
+            .frame(height: viewModel.isExpanded ? 400 : 40)
+          }
         }
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
       }
+      .navigationBarTitle(Text("Order book"))
     }
+    .tabItem({
+      Text("Order book")
+    })
   }
+  
+  
 }
